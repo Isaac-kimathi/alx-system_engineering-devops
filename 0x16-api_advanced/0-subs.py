7fdaed087f7eb45d7c12c43517a69f4c91f09dcb
Kimathi isaac <Isaackimathi@gmail.com>
2024-03-07 16:46:38 +0300

    queries the Reddit API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,12 +5,17 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    header = {
+            'User-Agent': 'Google Chrome Version 81.0.4044.129'
         }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+    except Exception:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
