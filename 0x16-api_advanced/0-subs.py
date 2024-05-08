0b5bda7453fb3470e1d7f25a8b2f1f3d291b7f38
Kimathi isaac <Isaackimathi@gmail.com>
2024-05-08 12:58:51 +0300

    number_of_subcribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
-import requests
-
+""" queries the Reddit API and returns the number of subscribers 
+(not active users, total subscribers) for a given subreddit."""
+from requests import get
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """function to perform the script documentation stipulation"""
     if subreddit is None or not isinstance(subreddit, str):
         return 0
-
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    header = {
-            'User-Agent': 'Google Chrome Version 81.0.4044.129'
-        }
-    response = requests.get(url, headers=header, allow_redirects=False)
+    user_agent = {'User-agent': 'Google Chrome Version 109.0.5414.165'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
     results = response.json()
-
     try:
         return results.get('data').get('subscribers')
     except Exception:
